0a7715ead93e990ce7cd8f4180a6fb4f3f566c22
ashnadsilva23 <ashnadsilva2309@gmail.com>
2024-06-11 22:57:59 +0530

    project completed

--- app.js
+++ app.js
@@ -1,27 +1,44 @@
 const express =  require("express")
 const mongoose = require("mongoose")
 const cors = require("cors")
+const {studentmodel} =require("./models/student")
+
+
 const app =express()
 app.use(cors())
+app.use(express.json())
+mongoose.connect("mongodb+srv://ashna:ashna@cluster0.n9qo4.mongodb.net/studentDb?retryWrites=true&w=majority&appName=Cluster0")
 
 
-app.get("/",(req,res)=>{
-    res.send("hello")
-    
-    
+
+
+
+
+
+app.post("/add",(req,res)=>{
+    let input= req.body
+    let student =  new studentmodel(input)
+student.save()
+    res.json({"status":"success"})
 })
 
-
-
-app.get("/contact",(req,res)=>{
-    res.send("Welcome to my contact page")
+app.post("/view",(req,res)=>{
+    studentmodel.find().then(
+        (data)=>{
+            res.json(data)
+        }
+    ).catch(
+        (error)=>{
+            res.json(error)
+        }
+    )
 })
+
 
 
 app.listen(8080,()=>{
     console.log("server started")
 })
-
 
 
 
